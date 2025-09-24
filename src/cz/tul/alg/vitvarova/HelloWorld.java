cc087381ae927b634e52bba8d578aec3e54e0138
Vitvarova-J-31c9 <Vitvarova-J-31c9@a2-06-mti.skynet.tul.cz>
2025-09-24 10:20:10 +0200

    Hello world

---
+++ src/cz/tul/alg/vitvarova/HelloWorld.java
@@ -0,0 +1,12 @@
+package cz.tul.alg.vitvarova;
+
+/**
+ *
+ * @author Vitvarova-J-31c9
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+            
+}
